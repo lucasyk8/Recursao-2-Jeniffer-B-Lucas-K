60322312e08ecbc13faf9d6577ff9f4e72f5861e
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-03-13 14:32:18 +0000

    resolucao fatorial

---
+++ Recursao2/src/App.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.*;
+
+public class App {
+    public static int Fatorial(int n){
+        if (n <= 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        return Fatorial(n) * Fatorial(n - 1);
+    }
+
+    public static int Somatoria(int n){
+        //tem condição de erro?
+        //tem condição especial?
+        //condição base pra recursão
+    }
+
+    public static int Fibonnaci(int n){
+        if (n <= 0){
+            return 0;
+        }    
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = 5;
+        System.out.println(Fatorial(n));
+        System.out.println(Somatoria(n));
+    }
+}
