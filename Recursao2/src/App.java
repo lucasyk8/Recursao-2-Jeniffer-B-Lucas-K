7e4e11269abdc4b1b24313c3899ff29fe640cc20
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-03-14 18:13:25 +0000

    ajuste na funcao somatorio2

--- Recursao2/src/App.java
+++ Recursao2/src/App.java
@@ -29,8 +29,14 @@ public class App {
         return  Fibonnaci(n - 1) +  Fibonnaci(n - 2);  
     }
 
-    public static int Somatorio2(int x, int y){
-        return 0;
+    public static int Somatorio2(int x, int y) {
+        if (x == 0 && y == 0) {
+            return 0;
+        }
+        if ((x == 1 && y == 0) || (x == 0 && y == 1)){
+            return 1;
+        } 
+        return x + y + Somatorio2(x -1 , y - 1);
     }
 
     public static void main(String[] args) throws Exception {
@@ -38,6 +44,6 @@ public class App {
         System.out.println("o fatorial de "+n+" é: "+Fatorial(n));
         System.out.println("o somatorio de "+n+" é: "+Somatorio(n));
         System.out.println("o fibonnaci de "+8+" é: "+Fibonnaci(8));
-        //System.out.println(Somatorio2(3, 3));
+        System.out.println("o somatorio de " + 5 + " e " + 5 + " é: " + Somatorio2(5, 5));
     }
 }
