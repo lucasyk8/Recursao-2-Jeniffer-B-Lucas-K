f9b54d026ce93e566dfd612312957bc42138cea5
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-03-14 17:58:51 +0000

    ajuste na funcao fibonnaci

--- Recursao2/src/App.java
+++ Recursao2/src/App.java
@@ -20,13 +20,13 @@ public class App {
     }
 
     public static int Fibonnaci(int n){
-        if (n <= 0){
+        if (n == 0){
             return 0;
         }
         if (n == 1){
             return 1;
         }
-        return Fibonnaci(n) + Fibonnaci(n + 1);  
+        return  Fibonnaci(n - 1) +  Fibonnaci(n - 2);  
     }
 
     public static int Somatorio2(int x, int y){
@@ -37,7 +37,7 @@ public class App {
         int n = 5;
         System.out.println("o fatorial de "+n+" é: "+Fatorial(n));
         System.out.println("o somatorio de "+n+" é: "+Somatorio(n));
-        //System.out.println(Fibonnaci(n));
+        System.out.println("o fibonnaci de "+8+" é: "+Fibonnaci(8));
         //System.out.println(Somatorio2(3, 3));
     }
 }
